dd8e0ddd65138a76debe1e141a7565e8361be3d2
Leon Lan <l.lan@vu.nl>
2023-12-02 22:45:17 +0100

    Complete get availability function

---
+++ tmb/__init__.py
@@ -0,0 +1,1 @@
+from .get_availability import get_availability
--- tmb/get_availability.py
+++ tmb/get_availability.py
@@ -1,19 +1,41 @@
-import datetime
+from datetime import datetime, timedelta
+import json
 
 import requests
 
 from .constants import API_URL
 
 
-def get_availability(date: str, id: int) -> dict[str, str]:
+def get_availability(date: str, id: int | str) -> dict[datetime, int]:
     """
-    Gets the availability of a given date and a given id
+    Gets the availability of a given date and a given id.
+
+    Parameters
+    ----------
+    date: str
+        The date in the format YYYY-MM-DD.
+    id: int | str
+        The id of the refuge.
+
+    Returns
+    -------
+    dict[datetime, int]
+        A dictionary with the bed availability for each day starting from
+        ``date`` until 30 days after.
     """
-    url = API_URL.format(date=date, id=id)
+    # The API returns availability for 15 days starting from ``date`` - 15 days.
+    # We need to add 15 days to the date to get the correct availability.
+    start_date = datetime.strptime(date, "%Y-%m-%d")
+    reference_date = (start_date + timedelta(days=15)).strftime("%Y-%m-%d")
+
+    url = API_URL.format(date=reference_date, id=id)
     response = requests.get(url)
 
-    breakpoint()
+    # Removing outer braces make the response a valid json
+    text = response.text.lstrip("(").rstrip(")")
+    data = json.loads(text)[0]
 
-    # 32405
-    # 2023-12-03
-    # object 15 is the first available date of miniapp of the website
+    return {
+        start_date + timedelta(days=idx): int(avail["s"])
+        for idx, avail in enumerate(data["planning"])
+    }
