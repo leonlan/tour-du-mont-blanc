e1b0deb1a8f84d72019fa9e4c70dede2203cf877
Leon Lan <l.lan@vu.nl>
2024-02-18 14:20:38 +0100

    Dashboard starters app

--- app.py
+++
@@ -1,9 +0,0 @@
-from shiny import render
-from shiny.express import input, ui
-
-ui.input_slider("n", "N", min=1, max=50, value=30)
-
-
-@render.code
-def txt():
-    return f"Hello! n*2 is {input.n() * 2}."
