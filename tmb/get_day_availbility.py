3a3be3bbec66baea07a3298fc0eb867fed984082
Leon Lan <l.lan@vu.nl>
2024-07-03 22:28:54 +0200

    Day availability stub

---
+++ tmb/get_day_availbility.py
@@ -0,0 +1,3 @@
+def get_day_availability(date, refuge_id: int):
+    """https://etape-rest.for-system.com/index.aspx/index.aspx?ref=json-produit-refuge&q=uk,32367,13/07/2024,1&callback=jQuery110207629344648364412_1720034293617&_=1720034293621"""
+    pass
