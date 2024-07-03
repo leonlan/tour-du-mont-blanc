bc510fde9e5c431b299c348b704d329fa79ad37a
Leon Lan <l.lan@vu.nl>
2024-07-03 22:34:19 +0200

    Add URLS

--- tmb/constants.py
+++ tmb/constants.py
@@ -1,1 +1,5 @@
 API_URL = "https://etape-rest.for-system.com/index.aspx/index.aspx?ref=json-planning-refuge&q={id},{date}"
+
+# https://etape-rest.for-system.com/index.aspx/index.aspx?ref=json-produit-refuge&q=fr,39948,14/07/2024,1
+# https://etape-rest.for-system.com/index.aspx/index.aspx?ref=json-produit-refuge&q=uk,32367,13/07/2024,1
+DAILY_AVAILABILITY_URL = "https://etape-rest.for-system.com/index.aspx/index.aspx?ref=json-produit-refuge&q=uk,{id},{date},1&callback=jQuery110207629344648364412_1720034293617&_=1720034293621"
