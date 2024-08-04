a68b51c2e09d8b8c7d406291806d9a9fafb7a9ba
Leon Lan <l.lan@vu.nl>
2024-08-04 12:37:48 +0200

    Add URL column, TMB ordering column, text color

--- app.py
+++ app.py
@@ -8,31 +8,27 @@ st.set_page_config(layout="wide")
 START_DATE = datetime.strptime("2024-06-01", "%Y-%m-%d")
 END_DATE = datetime.strptime("2024-09-29", "%Y-%m-%d")
 
+AVAILABILITY_URL = "https://raw.githubusercontent.com/leonlan/tour-du-mont-blanc/availability/daily/{date}.csv"
+HUTS_URL = "https://raw.githubusercontent.com/leonlan/tour-du-mont-blanc/main/data/huts.csv"
+
+
 today = datetime.today()
 
 try:
     # Get the most recent hut availability data.
-    df = pd.read_csv(
-        f"https://raw.githubusercontent.com/leonlan/tour-du-mont-blanc/availability/daily/{today.date()}.csv",
-        index_col=0,
-    )
+    df = pd.read_csv(AVAILABILITY_URL.format(date=today.date()), index_col=0)
     text = f"Data last updated on: {today.date()}."
 except:
     # Fallback to yesterday's data if today's data is not available.
-    yesterday = today - timedelta(days=1)
-    df = pd.read_csv(
-        f"https://raw.githubusercontent.com/leonlan/tour-du-mont-blanc/availability/daily/{yesterday.date()}.csv",
-        index_col=0,
-    )
-    text = f"Data last updated on: {yesterday.date()}"
+    yesterday = (today - timedelta(days=1)).date()
+    df = pd.read_csv(AVAILABILITY_URL.format(date=yesterday), index_col=0)
+    text = f"Data last updated on: {yesterday}"
 
-huts_df = pd.read_csv(
-    "https://raw.githubusercontent.com/leonlan/tour-du-mont-blanc/main/data/huts.csv",
-)
+
+huts_df = pd.read_csv(HUTS_URL)
 
 
 st.title("TMB hut availability calendar")
-
 
 date_range = st.date_input(
     "Select a date range:",
@@ -60,43 +56,51 @@ def highlight_conditions(val):
         color = "#FFEB3B"  # yellow
     else:
         color = ""
-    return f"background-color: {color}"
+    return f"background-color: {color}; color: black;"
 
 
-name2url = huts_df[["mon_name", "mon_url"]].set_index("mon_name")["mon_url"].to_dict()
+# Add the URL for each hut.
+cols = ["mon_name", "mon_url"]
+name2url = huts_df[cols].set_index("mon_name")["mon_url"].to_dict()
 url2name = {v: k for k, v in name2url.items()}
+df.insert(0, "URL", df.index.map(name2url))
+
+# Reorder on counter-clockwise order of huts.
+cc_ordering = huts_df["mon_name"].tolist()
+cc_ordering = {name: i for i, name in enumerate(cc_ordering, 1)}
+df.insert(1, "TMB ordering", df.index.map(cc_ordering))
 
 if len(date_range) == 2:  # both dates must be selected
-    cols = [
+    cols = ["URL", "TMB ordering"]
+    dates = [
         col
         for col in df.columns
-        if date_range[0] <= datetime.strptime(col, "%Y-%m-%d").date() <= date_range[1]
+        if col not in cols
+        and date_range[0]
+        <= datetime.strptime(col, "%Y-%m-%d").date()
+        <= date_range[1]
     ]
-    filtered_df = df.loc[selected_huts, cols]
+    filtered = df.loc[selected_huts, cols + dates]
 
-    if not filtered_df.empty:
-        display_df = filtered_df.copy()
-
-        # Turn hut name index into column and replace it with the URLs. Then
-        # style the column to display the hut names again, while making them clickable.
-        # Bit of a workaround but thats what they say in the docs: https://docs.streamlit.io/develop/api-reference/data/st.column_config/st.column_config.linkcolumn
-        display_df = display_df.reset_index().rename(columns={"index": "Hut"})
-        display_df["Hut"] = display_df["Hut"].map(name2url)
-
-        styled_df = display_df.style.format(
-            {"Hut": lambda url: url2name[url]},
+    if not filtered.empty:
+        display = filtered.copy()
+        styled = display.style
+        styled = styled.format(
+            {"URL": lambda _: "Link"},
             escape="html",
         )
-        styled_df = styled_df.map(highlight_conditions)
+        styled = styled.map(highlight_conditions, subset=dates)
 
         st.dataframe(
-            styled_df,
+            styled,
             column_config={
-                "Hut": st.column_config.LinkColumn("Hut"),
+                "URL": st.column_config.LinkColumn("Website"),
             },
-            hide_index=True,
         )
 
 
 st.markdown(text)
 st.markdown("Green: 4 or more beds available; Yellow: 1-3 beds available.")
+st.markdown(
+    "Click [here](https://umap.openstreetmap.fr/fr/map/tour-du-mont-blanc-ccpmb_206457#11/45.9051/6.9293) for a map of the TMB huts."
+)
